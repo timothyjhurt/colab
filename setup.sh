3c56867284370e3f2228bb8c616b55854bbf2b40
Timothy Hurt <timothyjhurt@gmail.com>
2019-01-14 15:27:42 -0800

    updating

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ wget -c https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh
 chmod +x Anaconda3-2018.12-Linux-x86_64.sh
 bash ./Anaconda3-2018.12-Linux-x86_64.sh -b -f -p /usr/local
 conda update -n base conda
-conda install -y --prefix /usr/local -c conda-forge cartopy
+conda install -c conda-forge cartopy
